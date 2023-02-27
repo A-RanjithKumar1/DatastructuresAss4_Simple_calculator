8935072d720367b29bbb863e1e893118a45f9137
Tringapps-Ranjithkumar <ananthula.kumar@tringapps.com>
2023-02-28 00:26:38 +0530

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,7 @@
 package org.example;
 import java.util.*;
 import java.util.logging.Logger;
-abstract  class Simplecalci{
-    private int num1;
-    private int num2;
-    abstract void add(int a, int b);
-    abstract void sub(int a, int b);
-    abstract void div(int a, int b);
-    abstract void mul(int a, int b);
-    public void setvalue(int n1,int n2){
-        this.num1=n1;
-        this.num2=n2;
-    }
-    public int getval1(){
-        return num1;
-    }
-    public int getval2(){
-        return num2;
-    }
-}
+
 public class Main extends Simplecalci {
     private  final Logger lOGGER =  Logger.getLogger("InfoLogging");
     double s=0;
